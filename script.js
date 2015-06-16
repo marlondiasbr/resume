8b8d3d08b3e4f1be6a96901971e41dc3be18060d
marlon.dias <marlon.dias@gmail.com>
2015-06-16 08:50:52 -0400

    mostra toFixed(2)

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 
 // Circle
-function Circle (radius) {
-	// propriedade
-    this.radius = radius;
-    // método para cálculo da área
-    this.area = function () {
-        return Math.PI * this.radius * this.radius;
-    };
-    // método para cálculo do perimetro
-    this.perimeter = function () {
-        return 2 * Math.PI * this.radius; 
+function Circle (r) {
+	// propriedade privada
+    var radius = r;
+    // método para atribuir novo Raio 
+    this.setRadius = function (newRadius) {
+		if ( isNaN(newRadius) ) { 
+			return false;
+		} else {
+			radius = newRadius;
+			return true;
+		}
     };     
+    
+	// método para recuperar Raio 
+    this.getRadius = function() { return radius; };
+
+	// método para cálculo da área
+    this.area = function () { return Math.PI * radius * radius; };
+
+	// método para cálculo do perimetro
+    this.perimeter = function () { return 2 * Math.PI * radius; };     
 }
 
 //Circle.prototype.getArea = function (circle,radius) {
